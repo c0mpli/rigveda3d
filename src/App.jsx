d0e467f210abd10c953aceb0bf629e1ef3ed6402
c0mpli <shirothegamer69@gmail.com>
2025-10-04 01:00:43 +0530

    atom added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as THREE from "three";
+import { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Trail, Float, Sphere, Stars } from "@react-three/drei";
+import { EffectComposer, Bloom } from "@react-three/postprocessing";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Canvas camera={{ position: [0, 0, 10] }}>
+      <color attach="background" args={["black"]} />
+      <Float speed={4} rotationIntensity={1} floatIntensity={2}>
+        <Atom />
+      </Float>
+      <Stars saturation={0} count={400} speed={0.5} />
+      <EffectComposer>
+        <Bloom mipmapBlur luminanceThreshold={1} radius={0.7} />
+      </EffectComposer>
+    </Canvas>
+  );
 }
 
-export default App
+function Atom(props) {
+  return (
+    <group {...props}>
+      <Electron position={[0, 0, 0.5]} speed={6} />
+      <Electron
+        position={[0, 0, 0.5]}
+        rotation={[0, 0, Math.PI / 3]}
+        speed={6.5}
+      />
+      <Electron
+        position={[0, 0, 0.5]}
+        rotation={[0, 0, -Math.PI / 3]}
+        speed={7}
+      />
+      <Sphere args={[0.35, 64, 64]}>
+        <meshBasicMaterial color={[6, 0.5, 2]} toneMapped={false} />
+      </Sphere>
+    </group>
+  );
+}
+
+function Electron({ radius = 2.75, speed = 6, ...props }) {
+  const ref = useRef();
+  useFrame((state) => {
+    const t = state.clock.getElapsedTime() * speed;
+    ref.current.position.set(
+      Math.sin(t) * radius,
+      (Math.cos(t) * radius * Math.atan(t)) / Math.PI / 1.25,
+      0
+    );
+  });
+  return (
+    <group {...props}>
+      <Trail
+        width={5}
+        length={10}
+        color={new THREE.Color(2, 1, 10)}
+        attenuation={(t) => t * t}
+      >
+        <mesh ref={ref}>
+          <sphereGeometry args={[0.25]} />
+          <meshBasicMaterial color={[10, 1, 10]} toneMapped={false} />
+        </mesh>
+      </Trail>
+    </group>
+  );
+}
